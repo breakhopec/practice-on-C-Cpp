b4e262639c88b969b73e2fb56a83a8a06a460feb
breakhopec <gouwenzhuo123@gmail.com>
2020-08-15 11:11:44 +0800

    8.15

---
+++ Python/PythonCrashCourse/chapter8/shirt.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+def main():
+    make_shirt('XXXL', 'A not pink shirt')
+
+def make_shirt(size, describe):
+    print('size: %s' % (size))
+    print('describe: %s' % (describe)) 
+
+if __name__ == '__main__':
+    main()
