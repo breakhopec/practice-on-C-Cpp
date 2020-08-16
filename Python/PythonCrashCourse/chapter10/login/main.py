f354dfb9d2043bd2f5efd10bbe640de1bb2a1f9b
breakhopec <gouwenzhuo123@gmail.com>
2020-08-16 23:51:15 +0800

    模拟的登录系统

---
+++ Python/PythonCrashCourse/chapter10/login/main.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+import login
+
+def main():
+    user1 = login.User()
+    user1.login()
+    #user2 = login.Admin()
+    #user2.make_user()
+
+if __name__ == '__main__':
+    main()
